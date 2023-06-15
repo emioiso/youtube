d7979215ac8da01754a75c90d9d14eb6f74945dd
emioiso <jijjjiijiijijiji@gmail.com>
2023-06-15 19:29:03 +0900

    memory_game(配列とオブジェクトの理解が深まったらもう一度やる！)

---
+++ tio/jikken/memory_game/memory_game.js
@@ -0,0 +1,83 @@
+const cards = {
+	first_one: false,
+	second_one: false
+};
+if(
+)
+
+// =====================================================
+		// let hart = null;
+		// let diamond = null;
+		// let count = 0;
+		// let checkedMatch = 0;
+		// let meDiamond = 0;
+		// let meHart = 0;
+
+		// const first1 = document.getElementById("first1")
+		// first1.addEventListener("click",openCardFirst1)
+		// 	function openCardFirst1(){
+		// 			alert('♡' + 1)
+		// 			hart = 1
+		// 			checkMatch()
+		// 		}
+
+		// const second1 = document.getElementById("second1")
+		// second1.addEventListener("click",openCardSecond)
+		// 	function openCardSecond(){
+		// 			alert('♢' + 1)
+		// 		diamond = 1
+		// 		checkMatch()
+		// 	}
+
+		// const first2 = document.getElementById("first2")
+		// first2.addEventListener("click",openCardFirst2)
+		// 	function openCardFirst2(){
+		// 			alert('♡' + 2)
+		// 			hart = 2
+		// 			checkMatch()
+		// 	}
+
+		// const second2 = document.getElementById("second2")
+		// second2.addEventListener("click",openCardSecond2)
+		// 	function openCardSecond2(){
+		// 			alert('♢' + 2)
+		// 			diamond = 2
+		// 			checkMatch()
+		// 	}
+
+		// function checkMatch(){
+		// 	if(hart === diamond){
+		// 		count += 1
+		// 		console.log(count)
+		// 		me += 1
+		// 	}
+		// 		if(count % 2 !== 0 ){
+		// 		alert(me + "ペア")
+		// 		count = 0
+		// 		hart = 0
+		// 		diamond = 0
+		// 		console.log(count)
+		// 		meDiamond = 1
+		// 		meHart = 1
+		// 		pairCheckedDelete()
+		// 		pairCheckedDelete2()
+		// 		}
+		// 	}
+
+		// function pairCheckedDelete() {
+		// 			var button = document.getElementById("first1");
+		// 			button.style.display = "none";
+		// 		}
+		// function pairCheckedDelete2() {
+		// 			var button2 = document.getElementById("second1");
+		// 			button2.style.display = "none";
+		// 		}
+		// function pairCheckedDelete() {
+		// 	var button = document.getElementById("first1");
+		// 	button.style.display = "none";
+		// }
+		// function pairCheckedDelete2() {
+		// 			var button2 = document.getElementById("second1");
+		// 			button2.style.display = "none";
+		// 		}
+		// =====================================================
